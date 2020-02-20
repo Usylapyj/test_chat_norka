aabda1aa436926b01f8c762818fc34e711772d12
Lis <novoselovvlad04@gmail.com>
2020-02-20 22:56:45 +0300

    123

--- server.py
+++ server.py
@@ -1,16 +1,9 @@
-#! /usr/bin/python
-
-import os
-import sys
 import socket
-import requests
-
-host = "aa.bb.cc.dd"
-port = 1234
-
-remote_ip = requests.get('https://ip.beget.ru/').text[:-1]
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((remote_ip, port))
-print(s.recv(1024))
+ 
+sock = socket.socket()
+sock.bind(("", 8303))
+while True:
+    sock.listen(1)
+    conn, addr = sock.accept()
+    data = conn.recv(128)
+    print("[" + addr[0] + "] " + bytes.decode(data))
