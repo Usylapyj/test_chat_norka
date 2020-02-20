ff054626b501ca2ebb786e95d0ec0a410b77e3ee
Lis <novoselovvlad04@gmail.com>
2020-02-20 15:52:56 +0300

    123

--- server.py
+++ server.py
@@ -3,11 +3,12 @@
 import os
 import sys
 import socket
+import requests
 
 host = "aa.bb.cc.dd"
 port = 1234
 
-remote_ip = socket.gethostbyname(host)
+remote_ip = requests.get('https://ip.beget.ru/').text[:-1]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
